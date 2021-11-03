9496c8ff387c4b60f1468443cfc7c05cf2a35f96
Arsh Pratap <62841337+arshPratap@users.noreply.github.com>
2021-11-03 18:38:32 +0530

    Added models for soil detection

---
+++ Soil/SoilType/SoilTypeModel.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SoilTypeModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,16,3,1)
+        self.conv2 = nn.Conv2d(16,32,3,1)
+        self.conv3 = nn.Conv2d(32,64,3,1)
+        self.conv4 = nn.Conv2d(64,64,3,1)
+        self.conv5 = nn.Conv2d(64,64,3,1)
+        self.fc1 = nn.Linear(1024,128)
+        self.fc2 = nn.Linear(128,5)
+    
+    def forward(self,X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X,2,2)
+        
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X,2,2)
+        
+        X = F.relu(self.conv3(X))
+        X = F.max_pool2d(X,2,2)
+        
+        X = F.relu(self.conv4(X))
+        X = F.max_pool2d(X,2,2)
+        
+        X = F.relu(self.conv5(X))
+        X = F.max_pool2d(X,2,2)
+        
+        X = X.view(-1,1024)
+        
+        X = F.relu(self.fc1(X))
+        X = self.fc2(X)
+        return F.log_softmax(X,dim=1)
